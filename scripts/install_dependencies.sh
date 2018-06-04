47b0d547753556c9e84f3b14b454d35f9768a6fb
sekharkanuri33 <39875905+sekharkanuri33@users.noreply.github.com>
2018-06-04 15:40:45 -0400

    new. add

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,5 +4,5 @@ export MOODLE_DOCKER_WWWROOT=/moodle-wwdata
 export MOODLE_DOCKER_DB=pgsql
 yum install -y telnet
 rm -rf /moodle-wwdata/*
-mv /tmp/new/* /moodle-wwdata
+cp -r /tmp/new/* /moodle-wwdata
 rm -rf /tmp/new/*
